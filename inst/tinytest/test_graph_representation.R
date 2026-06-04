bd04ff74e557a561a5d528bab82a381e01b6ee6c
pkb-pmj <pkb.pmj@gmail.com>
2026-06-04 18:25:30 +0200

    ✅ first tests of graph representation

---
+++ inst/tinytest/test_graph_representation.R
@@ -0,0 +1,20 @@
+library(graphs)
+
+g <- new_graph(10)
+
+from <- 1:9
+to <- 2:10
+weights <- runif(9)
+
+add_edges(g, from, to, weights)
+
+adj_list <- as.list(2:10)
+adj_list[[10]] <- numeric()
+expect_equal(as_adj_list(g), adj_list)
+
+edge_list <- data.frame(from = from, to = to, weight = weights)
+expect_equal(as_edge_list(g), edge_list)
+
+adj_matrix <- matrix(0, nrow = 10, ncol = 10)
+adj_matrix[cbind(from, to)] <- weights
+expect_equal(as_adj_matrix(g), adj_matrix)
