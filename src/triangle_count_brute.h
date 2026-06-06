6012c0fccc929c025c0554e6f3cee44cd1248acb
pkb-pmj <pkb.pmj@gmail.com>
2026-06-06 16:59:52 +0200

    💡 better description of complexity & edge/triangle order

--- src/triangle_count_brute.h
+++ src/triangle_count_brute.h
@@ -5,13 +5,14 @@
 size_t triangle_count_brute(const Graph& g) {
     size_t count = 0;
 
-    // O(V * deg(u) * deg(v) * log(deg(w))) -> O(V^3 * log(V))
+    // O(E * deg(v) * log(deg(w))) -> O(E * V * log(V))
     for (size_t u = 0; u < g.V(); u++) {
         for (auto v : g.vertices[u].neighbors) {
-            // only count each triangle once, in order u > v > w
+            // only count each edge once, in order u > v
             if (v >= u) break;
 
             for (auto w : g.vertices[v].neighbors) {
+                // only count each triangle once, in order u > v > w
                 if (w >= v) break;
                 if (g.vertices[w].has_neighbor(u)) count++;
             }
