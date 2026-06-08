e24e1a53a8b18e64c2a56e41624c7e13b7b2e276
pkb-pmj <pkb.pmj@gmail.com>
2026-06-08 12:09:13 +0200

    📝 document from_igraph

--- R/from_igraph.R
+++ R/from_igraph.R
@@ -1,3 +1,11 @@
+#' Creates graph from igraph object.
+#'
+#' @description Creates a graph object from an igraph object.
+#'
+#' Currently, internally uses igraph::as_edgelist and graphs::from_edge_list,
+#' but that might change and shouldn't be relied upon.
+#' @param ig igraph object
+#' @returns Graph object created from the igraph.
 from_igraph <- function(ig) {
   edge_list <- igraph::as_edgelist(ig)
   V <- max(edge_list)
