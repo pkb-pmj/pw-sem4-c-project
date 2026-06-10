83c40c164ffdc8141f32b87e562e043571210f88
pkb-pmj <pkb.pmj@gmail.com>
2026-06-10 21:28:34 +0200

    🚧 include UTC time in benchmark results filename

--- bench/suite/triangle_count.R
+++ bench/suite/triangle_count.R
@@ -18,6 +18,8 @@ results <- bench::press(
 
 commit <- system("git rev-parse --short HEAD", intern = TRUE)
 commit_idx <- system("git rev-list --count HEAD", intern = TRUE)
-filename <- paste0("bench/results/", Sys.Date(), "-sha", commit, ".rds")
+
+filename <- paste0("bench/results/", format(Sys.time(), tz = "Z"), " sha", commit, ".rds")
+
 results$commit_idx <- commit_idx
 saveRDS(results, file = filename)
