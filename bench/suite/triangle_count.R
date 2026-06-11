b02a5b9b085ffed4746e1aed21a6ff3fe18889ed
pkb-pmj <pkb.pmj@gmail.com>
2026-06-11 16:12:56 +0200

    🧱 benchmark online triangle counting

--- bench/suite/triangle_count.R
+++ bench/suite/triangle_count.R
@@ -5,14 +5,40 @@ edgelists <- readRDS("bench/data/edgelists.rds")
 metadata <- read.csv("bench/data/metadata.csv")
 
 igraphs <- lapply(edgelists, graph_from_edgelist)
-graphs <- lapply(igraphs, from_igraph)
+graphs_brute <- lapply(igraphs, from_igraph)
+graphs_intersect <- lapply(igraphs, from_igraph)
+graphs_online <- lapply(igraphs, from_igraph)
+
+edges <- lapply(metadata$name, function(name) {
+  set.seed(2137)
+  V <- metadata$vertices[metadata$name == name]
+  lapply(seq(10), function(x) sample.int(V, 2))
+})
+names(edges) <- metadata$name
 
 results <- bench::press(
   name = metadata$name,
   bench::mark(
-    igraph = sum(igraph::count_triangles(igraphs[[name]])) / 3,
-    brute = graphs::triangle_count(graphs[[name]], method = "brute"),
-    intersect = graphs::triangle_count(graphs[[name]], method = "intersect")
+    igraph = sapply(edges[[name]], function(edge) {
+      igraphs[[name]] <- igraph::add_edges(igraphs[[name]], edge)
+      sum(igraph::count_triangles(igraphs[[name]])) / 3
+    }),
+    brute = sapply(edges[[name]], function(edge) {
+      graphs::add_edge(graphs_brute[[name]], edge[1], edge[2])
+      graphs::triangle_count(graphs_brute[[name]], method = "brute")
+    }),
+    intersect = sapply(edges[[name]], function(edge) {
+      graphs::add_edge(graphs_intersect[[name]], edge[1], edge[2])
+      graphs::triangle_count(graphs_intersect[[name]], method = "intersect")
+    }),
+    online = {
+      sapply(graphs_online, graphs::enable_triangle_count)
+      sapply(edges[[name]], function(edge) {
+        graphs::add_edge(graphs_online[[name]], edge[1], edge[2])
+        graphs::triangle_count(graphs_online[[name]], method = "online")
+      })
+    },
+    check = FALSE
   )
 )
 
--- bench/view_results.R
+++ bench/view_results.R
@@ -16,7 +16,7 @@ results <- do.call(rbind, lapply(files, readResults))
 results <- results %>%
   mutate(
     method = as.factor(attr(expression, "description")),
-    result = as.integer(result),
+    # result = as.integer(result),
     median = as.numeric(median),
     min = as.numeric(min)
   )
@@ -25,10 +25,13 @@ results %>%
   group_by(method, name) %>%
   arrange(file) %>%
   mutate(idx = seq(n())) %>%
+  mutate(idx = idx - max(n() - idx) - 1) %>%
   ungroup() %>%
   ggplot(aes(x = idx, y = median, color = method, group = method)) +
   geom_line() +
   facet_wrap(vars(name), scales = "free") +
-  scale_y_log10()
+  scale_y_log10() +
+  theme(axis.ticks.x = element_blank(),
+        axis.text.x = element_blank())
 
 metadata <- read.csv("bench/data/metadata.csv")
